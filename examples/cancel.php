e1717b81e5cf227f1451c65e6854513b17ad3421
beratkrdr <beratkrdr@hotmail.com>
2021-09-06 15:23:02 +0300

    Update class

---
+++ examples/cancel.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../src/class.php";
+
+$yurtici = new yurticiKargo();
+
+$response = $yurtici->cancelShipment('123456');
+
+echo "<pre>";
+print_r($response);
+echo "</pre>";
+
+?>
